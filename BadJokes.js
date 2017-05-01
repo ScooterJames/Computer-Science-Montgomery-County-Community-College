74ba7b8a04386aaa88c2415965a838b84a89bd51
mmdobber538 <mdobbs8978@students.mc3.edu>
2017-05-01 12:07:41 -0400

    Update BadJokes.js

--- BadJokes.js
+++ BadJokes.js
@@ -7,19 +7,18 @@
 
 
 /**----- PiLexa BadJokes Resource ------*/
-
+/** Collects jokes into categories that users can call by addresssing the Alexa device */
 
 module.exports = { 
-    "bad":
+    "bad":    /**-- Selection of generally bad jokes--*/
 	[
         "If a bird of the sea is a seagull, does that mean a bird of the bay is a bagel? ",
 		"What is blue and smells like red paint? ... Blue Paint. ",
         "What's red and invisible? ... No Tomatoes. ",
-		"What kind of eel hits your eye like a big pizza pie? Thats a morray."
 	],
 		
     "stupid":
-	[
+	[	/**--selection of exceptionally stupid jokes  --*/
         "Three computer science students walk into a bar. The fourth one ducked. ",
 		"What do you call a bear with no teeth? ... A gummy bear. ",
         "A horse walks into a bar. The bartender asks, Why the long face? ",
@@ -27,7 +26,7 @@ module.exports = {
 	],
    
     "terrible":
-	[
+	[	/**-- A gloriously curated list of fabulously terrible jokes --*/
         "Two chemists are at a bar. One says I'd like some H 2 O. The other says I'd like some H 2 0 too. They have their drinks and the second man dies. ",
         "What's brown and sticky? ... A stick. ",
 		"Did you hear about the two antennas that got married? ... The ceremony was lame, but the reception was amazing. ",
@@ -35,28 +34,27 @@ module.exports = {
 	],
 
 	"dirty":
-	[
+	[	/**--a selection of not actually filthy jokes--*/
 		"I couldn't possibly do that.  ",
 		"Well, when an Alexa, Siri, and Cortana love each other very much, you're bound to get an improper response. ",
 		"You are fined one credit for violation of the verbal moralities code.  Be well. ",
 		" I'm happy that you're happy but the place where you're supposed to have the toilet paper you've got this little shelf with 3 seashells on it.",
 		
-		
 	],
 	
 	"pranks":
-	[
+	[	/**-- A selection of odd Alexa replies --*/
 		"Please say the last four digits of your card number to hear the next joke. ",
 		"Please describe what you are wearing, for government record. ",
 		"Battery low. Please recharge the Alexa unit. ",
 		"Holds up spork. ",
 		"Console gaming is inherently superior to PC. ",
-		"Steel beams can't melt dank memes. ",
+		"Dank memes can't melt steel beams. ",
 		"Somebody once told me the world is gonna roll me. ",
 	],
 	
 	"awful":
-	[
+	[	/**-- A selection of truly awful jokes or responses --*/
         "Your face. ",
 
         "INSERT JOKE. "
